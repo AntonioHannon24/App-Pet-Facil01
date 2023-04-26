91b73c8b2966ebbe830a02ecf74eb1a788c69f1f
Antonio <antoniohannon24@gmail.com>
2023-04-25 22:20:18 -0300

    confirmar

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { useColorScheme } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import themes from './src/theme'
 import Home from './src/Home';
-
+import { Text } from 'react-native';
 export default function App() {
 
   const deviceTheme = useColorScheme(); //dark,light, null undefined
