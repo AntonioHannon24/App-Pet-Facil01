3f00403c2f9957ef3878257ed714cb1e4ca4c68c
Antonio <antoniohannon24@gmail.com>
2023-05-02 18:34:59 -0300

    Arrumando função darkmode

--- App.js
+++ App.js
@@ -1,33 +1,16 @@
-import React, { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import themes from './src/theme'
 import Home from './src/Home';
-import { SwitchTheme, Container } from './styles';
+import { useColorScheme } from 'react-native';
 
 export default function App() {
 
-  const [isEnabled, setIsEnabled] = useState("dark");
-  const [backgroundTheme, setBackgroundTheme] = useState('light')
-
-  //função que muda o background 
-  const toggleSwitch = () => {
-    setIsEnabled(previousState => !previousState);
-    setBackgroundTheme(previousState => previousState === "dark" ? "light" : "dark")
-  };
-
-  const theme = themes[backgroundTheme]
+  const deviceTheme = useColorScheme(); // dark, light, null, undefined
+  const theme = themes[deviceTheme] || theme.dark
 
   return (
     <ThemeProvider theme={theme}>
-      <Container>
-        <SwitchTheme
-          trackColor={{ false: 'white', true: 'dark' }}
-          thumbColor={isEnabled ? 'white' : 'dark'}
-          onValueChange={toggleSwitch}
-          value={isEnabled}
-        />
-      </Container>
-      <Home />
+      <Home/>
     </ThemeProvider>
   );
 }
