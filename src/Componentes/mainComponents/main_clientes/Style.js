ef5bec4f55c065896a88907b0351ad0b5c01ebf3
DaviPadilha <padilhadavi11@gmail.com>
2023-05-05 22:57:44 -0300

    Primeira alteração na tela de Usuário.

---
+++ src/Componentes/mainComponents/main_clientes/Style.js
@@ -0,0 +1,35 @@
+
+import styled from 'styled-components/native';
+
+export const ContainerMain = styled.View`
+    flex: 1;
+  //justify-content: center;
+  //align-items: center;
+`;
+
+export const IconLogin = styled.Image`
+    width: 100px;
+    height: 100px;
+    margin-right: 8px;
+    margin-left: 20px;
+`;
+
+export const TextContainer = styled.View`
+    flex-direction: row;
+  //align-items: center;
+`;
+
+export const WelcomeText = styled.Text`
+    font-size: 15px;
+    font-weight: bold;
+`;
+
+export const PetContainer = styled.View`
+    flex-direction: row;
+`;
+
+export const PetText = styled.Text`
+    margin-right: 8px;
+    margin-left: 60px;
+
+`;
--- src/Componentes/mainComponents/main_clientes/index.js
+++ src/Componentes/mainComponents/main_clientes/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
-import { ContainerMain, Nome } from '../estilos_main.js'
+import {ContainerMain, IconLogin, TextContainer, WelcomeText, PetContainer, PetText} from "./Style";
 
 const TelaUser = () => {
-
+  const pets = ["Pet 1", "Pet 2", "Pet 3"]; // array com informações dos pets
 
   return (
     <ContainerMain>
-        <Nome>Tela de usuário</Nome>
+      <TextContainer>
+        <IconLogin source={require('../img/profile.png')} />
+        <WelcomeText>Bem-vindo usuário(a), e sua Pet Family</WelcomeText>
+      </TextContainer>
+      <PetContainer>
+        {pets.map((pet, index) => (
+          <PetText key={index}>{pet}</PetText>
+        ))}
+      </PetContainer>
     </ContainerMain>
   );
 };
 
 export default TelaUser;
-
 
