0d175f9920946df2f897a4cf47a6359623c9cba3
davi <davi@fertipar.com.br>
2023-11-03 13:08:33 -0300

    Ativação das rotas de login dos funcionários

--- src/Componentes/loginComponents/login_empresa/index.js
+++ src/Componentes/loginComponents/login_empresa/index.js
@@ -6,8 +6,8 @@ import { useForm, Controller } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { useEffect, useState } from 'react';
-
-
+import axios from 'axios';
+import URL from '../../../config';
 
 
 export default function LoginEstabelecimento  ({ navigation }) {
@@ -24,8 +24,42 @@ const [data,setData] = useState([])
     resolver: yupResolver(schema)
   });
 
-  function handleSignIn(data) { console.log(data) 
-    navigation.navigate('TelaEstabelecimentos')}
+  async function handleSignIn(data) {
+    console.log(data)
+    try {
+      const response = await axios.post(URL+'loginEstabelecimento',{
+        email: data.email,
+        password: data.password
+      });
+
+      if (response.status === 200) {
+        
+        // AsyncStorage.setItem('apiToken', response.data.token.token)
+        // AsyncStorage.setItem('type', response.data.token.type)
+        // AsyncStorage.setItem('idUser', response.data.user.id);
+        // AsyncStorage.setItem('sessao', 1); // 0 ou null que dizer que o usuário esta deslogado; 1  logado
+
+        navigation.navigate('TelaEstabelecimentos');
+        console.log(response.data.message);
+      } else if (response.status === 401) {
+        console.log(response.data.message);
+      } else {
+        console.log("Erro desconhecido");
+      }
+      console.log(response.data.message);
+    } catch (error) {
+      if (error.response) {
+        // O servidor respondeu com um código de erro (por exemplo, 401 não autorizado).
+        console.error('Erro no servidor:', error.response.data);
+      } else if (error.request) {
+        // A solicitação foi feita, mas não houve resposta do servidor.
+        console.error('Sem resposta do servidor');
+      } else {
+        // Ocorreu um erro ao configurar a solicitação.
+        console.error('Erro ao configurar a solicitação', error.message);
+      }
+    }
+  }
 
   return (
     <Container>
